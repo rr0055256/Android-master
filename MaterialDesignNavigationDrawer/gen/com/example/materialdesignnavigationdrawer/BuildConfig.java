3c5e5efb3e53a2fd1240f46cff1145a2fecbb220
rr0055256 <raph_rahul@yahoo.co.in>
2015-08-18 23:37:16 +0530

    New Folder for Android Projecst, PlayStoreStatus for offlineWebpage

---
+++ MaterialDesignNavigationDrawer/gen/com/example/materialdesignnavigationdrawer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.materialdesignnavigationdrawer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
